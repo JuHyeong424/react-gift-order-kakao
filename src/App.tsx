ed7e0f029910506e1b9df820fc05b763fa91a316
JuHyeong424 <144659958+JuHyeong424@users.noreply.github.com>
2025-07-02 17:15:50 +0900

    Revert "충남대 FE 장주형 - 0단계 기본 코드 준비 (#50)"
    This reverts commit b121af59f9d8d9f0bb0f125c5e2c5c0ad22f4b66.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Global, ThemeProvider } from '@emotion/react';
-import { JSX } from 'react';
-import Home from '@/pages/Home';
-import Login from '@/pages/Login/Login';
-import globalStyle from '@/styles/global';
-import { theme } from '@/styles/theme';
-import NotFound from '@/pages/NotFound/NotFound';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: () => JSX.Element = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Global styles={globalStyle} />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-      <ToastContainer position="top-center" autoClose={1000} />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import svgr from 'vite-plugin-svgr';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-    svgr(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+  plugins: [react()],
+})
