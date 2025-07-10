cb0821ab21c237bd542e30e5046cf65531f40cfc
JuHyeong <wkdwngud963@gmail.com>
2025-07-10 19:38:16 +0900

    refactor: Receiver 컴포넌트 기능별 분리

---
+++ src/components/Order/Receiver/ReceiverInput.tsx
@@ -0,0 +1,22 @@
+import { Input, ItemInput, ReceiverItem } from '@/components/Order/Receiver/Receiver.style.ts';
+
+export default function ReceiverInput({ type, label, name, placeholder,  register, validate, error }) {
+  return (
+    <ReceiverItem>
+      <span>{label}</span>
+      <ItemInput>
+        <Input
+          type={type}
+          {...register(name, {
+            required: `${label}를 입력해주세요`,
+            validate,
+          })}
+          isActive={error}
+          placeholder={placeholder}
+        />
+        {error?.message &&
+          <p>{error.message}</p>}
+      </ItemInput>
+    </ReceiverItem>
+  );
+}
