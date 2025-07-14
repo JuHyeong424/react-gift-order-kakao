13ca71dceb440692a904fa9c6535699e8ebe5f10
JuHyeong <wkdwngud963@gmail.com>
2025-07-14 19:39:32 +0900

    refactor: 사용하지 않은 불필요한 코드 및 파일 삭제

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,19 +1,5 @@
-export type MessageRef = {
-  triggerValidation: () => Promise<boolean>;
-  getMessage: () => string;
-};
-
 export type Receiver = {
   name: string;
   phone: string;
   count: number;
 }
-
-export type ReceiverRef = {
-  triggerReceiverValidation: () => Promise<boolean>;
-}
-
-export type SenderRef = {
-  triggerSenderValidation: () => Promise<boolean>;
-  getSender: () => string;
-};
